382629f8ce17711337d4a3160bf8b443d0c08bed
mihaicc30 <alemihai25@gmail.com>
2023-06-29 18:30:05 +0100

    fixed svg layout and changed color

--- resources/views/isAdmin/files.blade.php
+++ resources/views/isAdmin/files.blade.php
@@ -433,7 +433,8 @@ $receipts = [
 
                   <div class="flex flex-nowrap items-center ">
 
-                    <span class="text-xs" title="{{ $receipt['transfer_details']['file_size_original'] }}">{{ $receipt['transfer_details']['file_size_original'] }}</span>
+                    <span class="text-xs" title="{{ $receipt['transfer_details']['file_size_original'] }}">
+                      {{ $receipt['transfer_details']['file_size_original'] }}</span>
                     <span title="Download" class="p-1 m-1 cursor-pointer transition active:shadow-[inset_0px_1px_2px_2px_black] hover:scale-[1.1] ">
                       <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
