c572840521cc3f7005fe2c0c5f05a4570693ea88
mihaicc30 <alemihai25@gmail.com>
2023-08-29 09:54:26 +0100

    Provided info if customer account is locked.

---
+++ app/Http/Middleware/IsBlocked.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsBlocked
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check() && auth()->user()->IsBlocked()) {
+           
+            return $next($request);
+        }
+        if (auth()->check() && !auth()->user()->IsBlocked()) {
+            auth()->logout();
+            abort(423, 'Unauthorized action.');
+        }
+       
+    }
+}
